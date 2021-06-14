964c1dbe2048628dfa95f3948cb6bb4d88dc4238
agborges1986 <agborges1986@gmail.com>
2021-06-14 18:28:56 -0400

    first commit

---
+++ js/javascript.js
@@ -0,0 +1,24 @@
+
+api_key_weather="31ca2f423976763187fe613d8acb6ae4"
+
+$(document).ready(function () {
+    $('.btn').click(function () { 
+        city=$('.city_search').val();
+        
+        if (city=="") {
+            alert("Ingrese el nombre de una ciudad")
+        }else{
+        /* In this section  excecute de API call*/
+        url="http://api.openweathermap.org/data/2.5/weather?q="+city+"&&appid="+api_key_weather
+            /* alert(url) */
+            $.get(url, function(res) {
+                // your code here
+                tempF=((parseFloat(res.main.temp)-273.15)*9/5+32).toFixed(2)
+                
+                $('.city').text(res.name+": "+res.weather[0].main);
+                $('.temperature').text(tempF+"°F");
+                $('.location').html("Coordenadas: Longitud "+res.coord.lon+", Latitud: "+res.coord.lat);
+            }, 'json'); 
+        }
+    });    
+});
